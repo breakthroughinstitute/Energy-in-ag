0b66aa7d49a156af0586ef583f57982433ffffcf
danrejto1 <danrejto@gmail.com>
2024-01-12 17:24:07 -0800

    updated package from rgdal to sf for code for creating Africa shapefile

--- code/QGIS/create_africa_shapefile.R
+++ code/QGIS/create_africa_shapefile.R
@@ -1,11 +1,11 @@
-library(rgl)
-library(rgdal)
-library(raster)
+#load a package to read in and modify shapefiles
+library(sf)
 
-setwd("/Volumes/GoogleDrive/My Drive/BTI Research/Food & Farming/Energy in ag/Energy and GHG Analysis (2019)/Analysis/Calc LUC_EnergyAg/QGIS_MAPSPAM_analysis")
-shp <- readOGR(dsn="ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
+#read the shapefile 
+shp <- st_read("raw_data/qgis/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
 
-africa_shp <- subset(shp, CONTINENT=="Africa")
+#subset to Africa
+africa_shp <- shp[shp$CONTINENT == "Africa", ]
 
-## Store the Africa shapefile so that you don't have to import the whole world next time:
-writeOGR(africa_shp, ".", "africa-rgdal", driver="ESRI Shapefile")
+# Save the Africa shapefile so that you don't have to import the whole world next time:
+st_write(africa_shp, "raw_data/qgis/africa-rgdal", driver="ESRI Shapefile")
