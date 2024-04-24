64c31455415af99743f71feee803dc673bd5d700
Linus Blomqvist <linus@ucsb.edu>
2024-04-23 17:25:25 -0700

    Created data exploration script

---
+++ code/linus_data_exploration.R
@@ -0,0 +1,43 @@
+eastern_df <- filter_reg %>% filter(country == "Western")
+lm_all <- lm(filter_reg, formula = log(fert_emiss_ha) ~ log(yield) + factor(country))
+lm_eastern <- lm(eastern_df, formula = log(fert_emiss_ha) ~ log(yield))
+
+lm_all$coefficients[2] # 1.353147
+lm_eastern$coefficients[2] # 2.191207
+
+pred_eastern_with_all <- predict(lm_all, newdata = eastern_df)
+pred_eastern_itself <- predict(lm_eastern, newdata = eastern_df)
+
+eastern_df <- data.frame(eastern_df, pred_with_all = exp(pred_eastern_with_all), 
+                         pred_itself = exp(pred_eastern_itself))
+
+eastern_df <- eastern_df %>%
+  select(country, year, yield, fert_emiss_ha, pred_with_all, pred_itself) %>%
+  pivot_longer(!c(country, year, yield), names_to = "scenario", values_to = "fert_emiss")
+
+eastern_df %>%
+  ggplot(aes(x = log(yield), y = log(fert_emiss), col = scenario)) +
+  geom_line()
+
+####
+filter_reg %>%
+  filter(year == 2020) %>%
+  ggplot(aes(x = yield, y = fert_emiss_ha)) +
+  geom_point() +
+  geom_smooth(method = "gam")
+
+lm_2020 <- lm(filter_reg %>% filter(year == 2020), formula = log(fert_emiss_ha) ~ log(yield)) 
+lm_2020$coefficients[2] # 0.7722751
+
+lm_1970 <- lm(filter_reg %>% filter(year == 1970), formula = log(fert_emiss_ha) ~ log(yield)) 
+lm_1970$coefficients[2] # 0.9077005
+
+lm_2000 <- lm(filter_reg %>% filter(year == 2000), formula = log(fert_emiss_ha) ~ log(yield)) 
+lm_2000$coefficients[2] # 0.8281306
+
+lm_all$coefficients[2]
+
+filter_reg %>%
+  ggplot(aes(x = log(yield), y = log(fert_emiss_ha))) +
+  geom_point() +
+  geom_smooth(method = "lm")
